735d9451264c21a568e04ca81d9d2736ddc2316a
matvey <bme15122000@gmail.com>
2020-09-22 01:46:24 +0300

    initial commit

---
+++ WayCo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WayCo
+//
+//  Created by  Matvey on 22.09.2020.
+//  Copyright © 2020 Borisov Matvei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
